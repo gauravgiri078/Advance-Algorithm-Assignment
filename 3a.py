2cf13f0e1105f9283a67bc0b1ec4790e6f60088b
gauravgiri078 <gauravgiri078@gmail.com>
2026-01-21 12:47:59 +0545

    2a added

--- 3a.py
+++ 3a.py
@@ -1,61 +1,52 @@
 class Node:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+	def __init__(self, val=0, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
 
 
 def solve(root):
-    ans = 0
-class Node:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+	ans = 0
 
+	def dfs(node):
+		nonlocal ans
+		if not node:
+			return 2
+		L = dfs(node.left)
+		R = dfs(node.right)
+		if L == 0 or R == 0:
+			ans += 1
+			return 1
+		if L == 1 or R == 1:
+			return 2
+		return 0
 
-def solve(root):
-    ans = 0
-
-    def dfs(node):
-        nonlocal ans
-        if not node:
-            return 2
-        l = dfs(node.left)
-        r = dfs(node.right)
-        if l == 0 or r == 0:
-            ans += 1
-            return 1
-        if l == 1 or r == 1:
-            return 2
-        return 0
-
-    if dfs(root) == 0:
-        ans += 1
-    return ans
+	if dfs(root) == 0:
+		ans += 1
+	return ans
 
 
 def build_tree(arr):
-    if not arr:
-        return None
-    nodes = [None if v is None else Node(v) for v in arr]
-    n = len(arr)
-    for i in range(n):
-        if nodes[i] is not None:
-            li = 2 * i + 1
-            ri = 2 * i + 2
-            if li < n:
-                nodes[i].left = nodes[li]
-            if ri < n:
-                nodes[i].right = nodes[ri]
-    return nodes[0]
+	if not arr:
+		return None
+	nodes = [None if v is None else Node(v) for v in arr]
+	for i in range(len(arr)):
+		if nodes[i] is not None:
+			l = 2 * i + 1
+			r = 2 * i + 2
+			if l < len(arr):
+				nodes[i].left = nodes[l]
+			if r < len(arr):
+				nodes[i].right = nodes[r]
+	return nodes[0]
 
 
-if __name__ == "__main__":
-    arr = [0, 0, None, 0, None, 0, None, None, 0]
-    root = build_tree(arr)
-    print(solve(root))
-    
-    
-    print("Time Complexity: O(N)")
-    print("Space Complexity: O(H)")
+if __name__ == '__main__':
+	# test: tree = [0,0,null,0,null,0,null,null,0]
+	arr = [0, 0, None, 0, None, 0, None, None, 0]
+	root = build_tree(arr)
+	res = solve(root)
+	print(res)
+	print('Time Complexity: O(N)')
+	print('Space Complexity: O(H)')
+
